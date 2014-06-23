94b70b316030341283c5d98daa19b0213d2321a4
Eric Coffman <eric@coffman.net>
2014-06-23 17:14:06 -0400

    Update nuget package

--- Dapper.SimpleCRUD/Properties/AssemblyInfo.cs
+++ Dapper.SimpleCRUD/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Eric Coffman")]
 [assembly: AssemblyProduct("Dapper.SimpleCRUD")]
-[assembly: AssemblyCopyright("Copyright Eric Coffman © 2013")]
+[assembly: AssemblyCopyright("Copyright Eric Coffman © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.4.1.0")]
+[assembly: AssemblyFileVersion("1.4.1.0")]
